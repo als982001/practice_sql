cbbb6ff3b1379d0857c50d98b16c161c9898773a
jaemin <als982001@naver.com>
2025-04-13 14:46:04 +0900

    function, procedure

--- practice_postgre.sql
+++ practice_postgre.sql
@@ -109,20 +109,145 @@ FROM movies
 
 ALTER TABLE movies DROP COLUMN genres;
 
+-- Function, Procedure 연습
+
+CREATE OR REPLACE FUNCTION hello_world()
+RETURNS TEXT AS 
+$$
+	SELECT 'hello_world';
+$$
+LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION hello_world(user_name TEXT)
+RETURNS TEXT AS 
+$$
+	SELECT 'hello ' || user_name;
+$$
+LANGUAGE SQL;
+
+-- 2개의 TEXT를 입력으로 받음
+-- parameter에 이름이 없기 때문에 위치로 나타낼 수 있음
+CREATE OR REPLACE FUNCTION hello_world(TEXT, TEXT)
+RETURNS TEXT AS 
+$$
+	SELECT 'hello ' || $1 || ' and ' || $2;
+$$
+LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION hello_world_user(user_name TEXT)
+RETURNS TEXT AS 
+$$
+	SELECT 'hello ' || user_name;
+$$
+LANGUAGE SQL;
+
+SELECT hello_world();
+SELECT hello_world_user('nico') FROM movies;
+
+-- PostgreSQL은 function의 이름과 입력값, 출력값을 모두 보기 때문에 전부 다 다른게 실행됨.
+SELECT hello_world() FROM movies;
+SELECT hello_world('nico') FROM movies;
+SELECT hello_world('nico', 'james') FROM movies;
+
+CREATE OR REPLACE FUNCTION is_hit_or_flop(movie movies)
+RETURNS TEXT AS
+$$
+	SELECT CASE
+  	WHEN movie.revenue > movie.budget THEN 'Hit'
+    WHEN movie.revenue < movie.budget THEN 'Flop'
+    ELSE 'N/A'
+  END
+$$
+LANGUAGE SQL;
+
+DROP FUNCTION is_hit_or_flop(movies);
+
+/*
+-- 함수 기본 설정: VOLATILE -> LANGUAGE SQL VOLATILE;
+-- volatile function은 같은 인자를 넣어도 다른 결과를 return할 수 있음
+-- stable function: database를 수정할 수 없는 function. 단일 구문 내의 모든 row에 동일한 argument에 대해서 같은 결과를 return한다.
+-- immutable function: database 수정이 불가능하고 동일한 argument가 주어질 경우 영원히 같은 결과를 return 한다.
+*/
+
+CREATE OR REPLACE FUNCTION is_hit_or_flop(movie movies)
+RETURNS TABLE (hit_or_flop TEXT, other_thing NUMERIC) AS
+$$
+	SELECT CASE
+  	WHEN movie.revenue > movie.budget THEN 'Hit'
+    WHEN movie.revenue < movie.budget THEN 'Flop'
+    ELSE 'N/A'
+  END, 11111;
+$$
+LANGUAGE SQL IMMUTABLE;
+
+SELECT 
+	title, 
+  (is_hit_or_flop(movies.*)).*
+FROM movies;
+
+CREATE OR REPLACE FUNCTION set_updated_at()
+RETURNS TRIGGER AS
+$$
+	BEGIN
+  	NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+  END
+$$
+LANGUAGE PLPGSQL; -- plpgsql: PostgreSQL이 지원하는 언어로, Procedual Language PostgreSQL을 의미함.
+
+CREATE TRIGGER updated_at
+BEFORE UPDATE
+ON movies
+FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
 
 
+-- function은 무엇인가를 return하도록 되어 있다. procedure은 return할 필요가 없다.
+-- function은 DML command 안에서 호출된다. procedure은 DML command에서 호출되지 않고, CALL 뒤에 procedure의 이름을 붙여서 호출함
 
+CREATE PROCEDURE set_zero_revenue() AS
+$$
+	UPDATE movies SET revenue = NULL WHERE revenue = 0;
+$$
+LANGUAGE SQL;
 
+CALL set_zero_revenue();
 
+-- 입력값, 출력값 같으면 INOUT 이용 가능
+CREATE PROCEDURE hello_world_p(IN name TEXT, OUT greeting TEXT) AS
+$$
+	BEGIN
+  	greeting = 'Hello ' || name;
+  END;
+$$
+LANGUAGE PLPGSQL;
+                                                           
+CALL hello_world_p('nico', NULL);
 
+CREATE EXTENSION plpython3u;
 
+CREATE OR REPLACE FUNCTION hello_world_py(name TEXT)
+RETURNS TEXT AS
+$$
+  return f'hello {name}'
+$$
+LANGUAGE plpython3u;
 
+CREATE OR REPLACE FUNCTION log_updated_at_py()
+RETURNS TRIGGER AS
+$$
+    import json, requests
+    requests.post(
+        'http://localhost:3000',
+        data=json.dumps({'td': TD}),
+        headers={'Content-Type': 'application/json'}
+    )
+    return NEW
+$$
+LANGUAGE plpython3u;
 
+SELECT hello_world_py('nico');
 
-
-
-
-
-
-
-
+CREATE TRIGGER updated_at_py
+BEFORE UPDATE
+ON movies
+FOR EACH ROW EXECUTE PROCEDURE log_updated_at_py();
